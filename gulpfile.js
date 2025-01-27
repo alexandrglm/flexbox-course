c3f82f78772c0ba398df0a735997f53266baa63d
Alexandr Gomez <undefined.platform159@passinbox.com>
2025-01-27 07:59:32 +0100

    Created a 404 error page to show how to set a simple Flexbox aligned items.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
 var gulp = require("gulp");
-var browserSync = require("browser-sync").create();
-var reload = browserSync.reload;
+var dirBase = require('browser-sync').create();
+const browserSync = require("browser-sync");
 
 gulp.task("serve", function() {
   browserSync.init({
     server: {
-      baseDir: "./"
-    }
+      baseDir: dirBase.resolve(__dirname, "./")
+    },
+    logLevel: "debug"
   });
 
-  gulp.watch("*.html").on("change", reload);
+  gulp.watch("*.html").on("change", browserSync.reload);
 });
