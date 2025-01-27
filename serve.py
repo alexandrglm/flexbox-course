77d1feb1fbcdd386301a9b52c7dba32bdf958c88
Alexandr Gomez <alexandrglm@@users.noreply.github.com>
2025-01-27 10:51:03 +0100

    serve_and_reload.py: A way to get things harder with no reason (testin purposes).

--- serve.py
+++ serve.py
@@ -2,20 +2,31 @@ import os
 import webbrowser
 from threading import Timer
 from flask import Flask, send_from_directory
+from flask_socketio import SocketIO
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
 
 app = Flask(__name__)
 currentPath = os.path.dirname(os.path.abspath(__file__))
 serveFile = 'index.html'
+socketIo = SocketIO(app, async_mode="eventlet")
 
 
 @app.route('/')
 def index():
     return send_from_directory(currentPath, serveFile)
 
+
 def serve():
     webbrowser.open("http://localhost:5000")
 
+@socketIo.on('reload')
+def reloadChanges():
+    print("Changes detected, reloading...")
+
+
 if __name__ == '__main__':
     Timer(1, serve).start()
-    app.run(debug=True, use_reloader=True)
+    socketIo.run(app, debug=True)
 
